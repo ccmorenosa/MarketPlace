d8e27808f1aedb6623e96f562ce5eaf3a8f1fd0d
Catalina Sarria <cimorenos@unal.edu.co>
2023-03-06 18:15:11 -0500

    Change extension to JSX

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+      <div className="h-screen w-screen bg-emerald-300">
+        <h1 className="text-3xl font-bold underline text-blue">
+          Hello world!
+        </h1>
+
+        <h2 className="text-xl text-green-t5">
+          test
+        </h2>
+      </div>
+  );
+}
+
+export default App;
